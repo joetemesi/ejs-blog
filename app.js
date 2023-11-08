60a114a12da2b04792948d558b89e406b68def0d
Joseph M. Temesi <joemunyendo@gmail.com>
2023-11-08 14:04:06 +0300

    Worked on navbar links

--- app.js
+++ app.js
@@ -19,7 +19,23 @@ app.use(express.static("public"));
 app.get("/", function(req, res){
 
   res.render("home", {
-    paragraph: homeStartingContent 
+    startingContent: homeStartingContent 
+  })
+
+})
+
+app.get("/about", function(req, res){
+
+  res.render("about", {
+    aboutContent: aboutContent 
+  })
+
+})
+
+app.get("/contact", function(req, res){
+
+  res.render("contact", {
+    contactContent: contactContent 
   })
 
 })
